949ed8d213c8763c0a82ac1a50e5ba97fd12df80
cobrianz <briancheruiyot.h@gmail.com>
2024-07-29 21:34:40 +0300

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
         </ul>
       </div>
       <div className="navbar-right">
-        
+        <a href="loginn"></a>
         <input type="search"/>
         <span>Become a seller</span>
         <button>Sign In</button>
